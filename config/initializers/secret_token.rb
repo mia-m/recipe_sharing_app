3fbc0bc0d2a9305101de894d74775518091cedd7
Mia Manalastas <miyahmi@gmail.com>
2013-09-07 14:04:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipeSharingApp::Application.config.secret_token = '7bc757791867f6ef5f20c21c04a27f04691ac342512822b5d492071d9759af2aca97e4f8f1adcc2289d4ddb13fa989e0bbcad0504a846a99e212a3687a665b13'
